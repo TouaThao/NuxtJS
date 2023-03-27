fb8a6c3a21199fd558a7371bface8f45689ece73
TouaThao <touathao09@gmail.com>
2023-03-27 01:35:12 -0500

    removed data

--- server/api/nuxtjs.js
+++ server/api/nuxtjs.js
@@ -5,7 +5,3 @@ export default defineEventHandler(async (event) => {
 
   return data;
 });
-
-// tVcQKY2caizoHPpKkJZwBzB9sUoKPoxWrAY1PFy8
-
-// https://api.currencyapi.com/v3/latest?apikey=tVcQKY2caizoHPpKkJZwBzB9sUoKPoxWrAY1PFy8
